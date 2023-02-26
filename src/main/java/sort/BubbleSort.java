f4a8135645210b69551faba10293b8b84301e181
钟亨 <260789672@qq.com>
2023-02-26 17:51:03 +0800

    修复BubbleSort冒泡排序算法

--- src/main/java/sort/BubbleSort.java
+++ src/main/java/sort/BubbleSort.java
@@ -1,19 +1,20 @@
 package sort;
+
 /**
  * 冒泡
  */
 public class BubbleSort {
 
-	public static void main(String[] args) {
-		int[] a = ExampleArray.A;
-		int length = a.length;
-		for(int i = 1; i < length; i++){
-			for(int j = length - i; j > 0; j--){
-				if(a[j] > a[j - 1]){
-					Util.swap(a, j, j-1);
-				}
-			}
-		}
-		Util.print(a);
-	}
+    public static void main(String[] args) {
+        int[] a = ExampleArray.A;
+        int length = a.length;
+        for (int i = 0; i < length; i++) {
+            for (int j = length - 1; j > i; j--) { // j-1边界条件要判断到a[0], 等于0时是j=i
+                if (a[j] < a[j - 1]) {
+                    Util.swap(a, j, j - 1);
+                }
+            }
+        }
+        Util.print(a);
+    }
 }
